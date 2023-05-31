06efa2649597e66720ee28ea38e28d2ad022f3a9
jules <jules.ferlin@gmail.com>
2023-05-31 22:17:11 +0200

    deleted comments

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include "distanceMatrix.h"
 
 int main() {
-  /*
-  readCSVFile file("../instances/94Missions-3centres/centers.csv");
-  file.printTable();
-  */
 
   distanceMatrix matrix("../instances/30Missions-2centres/");
   matrix.printMatrix();
