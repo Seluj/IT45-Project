ec4394f57b7cb3597e7649eef90caee1a817e478
Céliane <celianeallaire@gmail.com>
2023-06-03 15:13:49 +0200

    début ajout fonctions vides dans kMedoids

--- kMedoids.cpp
+++ kMedoids.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "kMedoids.hpp"
+
+/* ----------------------------------- KMedoid ----------------------------------- */
+  
+  /**
+   * @brief Function that applies the k-medoids algorithm
+   * @param data1 Data object to use
+   */
+  void kMedoids::kMedoidsAlgo(data* data1){
+
+  }
+
+  /**
+   * @brief Function that assigns each mission to a center
+  */
+  void kMedoids::medoidsAssign(){
+
+  }
+
+  //Est ce que l'on update les medoids et on fait plusieurs itérations où est ce que l'on fait une seule itération ?
+  //Ou sinon juste à chaque affectation d'une mission on update les medoids ?
+  //Sinon retour sur l'utilisation de k-means (qui coute moins chère en compilation) et juste on calcul la position de chaque points
+  void kMedoids::medoidsUpdate(){
+    
+  }
+    
+  void kMedoids::calculateCost(){
+
+  }
