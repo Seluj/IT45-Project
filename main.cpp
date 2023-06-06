7aa88b2c449d25bfdaace27c7a26b1646b6ea3b7
jules <jules.ferlin@gmail.com>
2023-06-06 13:04:03 +0200

    added argument parsing to the main function

--- main.cpp
+++ main.cpp
@@ -3,16 +3,30 @@
 #include "data.hpp"
 #include "dataAnalysis.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  auto *dataColl = new dataCollection("../instances/30Missions-2centres/");
+  std::string path = "./instances/30Missions-2centres/";;
+
+  if (argc == 1) {
+    printf("Take default path : ./instance/30Missions-2centres/\n");
+  } else if (argc == 2){
+    printf("Take path : %s\n", argv[1]);
+    path = argv[1];
+  } else {
+    printf("To many arguments\n");
+    printf("Take default path : ./instance/30Missions-2centres/\n");
+  }
+
+  printf("Press enter to continue");
+  getchar();
+
+
+  auto *dataColl = new dataCollection(path);
   dataColl->printDataCollection();
 
   auto *data1 = new data();
   auto *dataAnalysis1 = new dataAnalysis(dataColl, data1);
   dataAnalysis1->dataAnalysisPrint(data1);
-
-
 
   return 0;
 }
