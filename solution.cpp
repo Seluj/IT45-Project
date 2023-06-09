2969339a0a54485d23ba57d4a1e2e8f4b8b67b74
jules <jules.ferlin@gmail.com>
2023-06-10 00:32:44 +0200

    added and removed print function

--- solution.cpp
+++ solution.cpp
@@ -10,8 +10,6 @@ solution::solution(data *data) {
   for (int i = 0; i < data->nbEmployees; i++) {
     for (int j = 0; j < data->nbMissions; j++) {
       if (data->employees[i]->getSkill() == data->missions[j]->getSkill()) {
-        std::cout << data->employees[i]->getSkill() << data->employees[i]->getId() << " is assigned to mission "
-                  << data->missions[j]->getId() << std::endl;
         this->affectations[data->employees[i]->getSkill()][data->employees[i]->getId()][data->missions[j]->getId()] = false;
       }
     }
@@ -27,17 +25,17 @@ solution::solution() {
 solution::~solution() = default;
 
 void solution::printSolution(data *data) {
-  std::cout << "Solution : " << std::endl;
+  std::cout << std::endl << "------------------------- Solution : ------------------------- " << std::endl;
   std::vector<std::string> skills = {"LSF", "LPC"};
 
   std::cout << "Size of the affectations table : " << affectations.size() << std::endl;
   std::cout << "Size of the affectations table for LSF : " << affectations["LSF"].size() << std::endl;
+  std::cout << "Size of the affectations table for LSF and an employee : " << affectations["LSF"][1].size() << std::endl;
   std::cout << "Size of the affectations table for LPC : " << affectations["LPC"].size() << std::endl;
-
-
+  std::cout << "Size of the affectations table for LPC and an employee : " << affectations["LPC"][2].size() << std::endl;
 
   for (const auto & skill : skills) {
-    std::cout << "Table for skill " << skill << std::endl;
+    std::cout << "Table for skill : " << skill << std::endl;
     for (int j = 0; j < data->nbEmployees; j++) {
       if (data->employees[j]->getSkill() == skill) {
         std::cout << "Employee " << data->employees[j]->getId() << " : ";
