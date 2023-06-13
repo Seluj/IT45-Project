61ca5535e8cc9f6dd9b5f67cd810bb410f0b589b
jules <jules.ferlin@gmail.com>
2023-06-13 10:29:22 +0200

    modified main function

--- main.cpp
+++ main.cpp
@@ -26,13 +26,61 @@ int main(int argc, char *argv[]) {
 
 
   auto *dataColl = new dataCollection(path);
-  dataColl->printDataCollection();
 
   auto *data1 = new data();
   auto *dataAnalysis1 = new dataAnalysis(dataColl, data1);
   dataAnalysis1->dataAnalysisPrint(data1);
-  kMedoids *kMedoids1 = new kMedoids(data1);
+
+  auto *kMedoids1 = new kMedoids(data1);
   kMedoids1->kMedoidsAlgo(data1);
 
+  auto *genetic1 = new genetic();
+
+  auto *solution1 = new solution(data1);
+  auto *solution2 = new solution(data1);
+  auto *solution3 = new solution(data1);
+  auto *solution4 = new solution(data1);
+  auto *solution5 = new solution(data1);
+
+  solution1->initialSolution(data1);
+
+  std::cout << "Solution 1" << std::endl;
+  solution1->printSolution(data1);
+
+  /* --------------------------------------- Mutation 1 --------------------------------------- */
+
+  data1 = genetic1->mutateData(data1);
+  std::cout << "Mutate data done 1" << std::endl;
+  solution2->initialSolution(data1);
+  std::cout << "Solution 2" << std::endl;
+  solution2->printSolution(data1);
+
+  /*
+  *//* --------------------------------------- Mutation 2 --------------------------------------- *//*
+
+  data1 = genetic1->mutateData(data1);
+  std::cout << "Mutate data done 2" << std::endl;
+  solution3->initialSolution(data1);
+  std::cout << "Solution 3" << std::endl;
+  solution3->printSolution(data1);
+
+  *//* --------------------------------------- Mutation 3 --------------------------------------- *//*
+
+  data1 = genetic1->mutateData(data1);
+  std::cout << "Mutate data done 3" << std::endl;
+  solution4->initialSolution(data1);
+  std::cout << "Solution 4" << std::endl;
+  solution4->printSolution(data1);
+
+  *//* --------------------------------------- Mutation 4 --------------------------------------- *//*
+
+  data1 = genetic1->mutateData(data1);
+  std::cout << "Mutate data done 3" << std::endl;
+  solution5->initialSolution(data1);
+  std::cout << "Solution 5" << std::endl;
+  solution5->printSolution(data1);
+*/
+  printf("Press enter to continue");
+  getchar();
   return 0;
 }
