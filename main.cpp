f9ca4c0ff7bde4d8f04cdfadd4377dbbf7516d31
jules <jules.ferlin@gmail.com>
2023-06-14 19:06:03 +0200

    upload main.cpp : reduce code

--- main.cpp
+++ main.cpp
@@ -22,21 +22,39 @@ int main(int argc, char *argv[]) {
     std::cout << "Take default path: " << path << std::endl;
   }
 
-  printf("Press enter to continue");
-  getchar();
 
-  printf("Would you like the details for the kMedoid operations? [enter 1 for yes and 0 for no]\n");
+  std::cout << "Would you like the details for the kMedoid operations? [enter 1 for yes and 0 for no]";
   std::cin >> printDetails;
   while (printDetails != 0 && printDetails != 1) {
-    printf("Please enter either 1 or 0 [enter 1 for yes and 0 for no]\n");
-    std::cin >>printDetails;
+    std::cout << "Please enter either 1 or 0 [enter 1 for yes and 0 for no]";
+    std::cin >> printDetails;
   }
-  
+
+  std::cout << "How many solutions would you like to generate? (Between 1 and 50)" << std::endl
+            << "(The more solutions, the longer the program will take and the greater the risk of a stack overflow will occur)"
+            << std::endl << "(If you see \"Best solution\", the program is still running)";
+  std::cin >> nbSolutions;
+  while (nbSolutions < 1 || nbSolutions > 50) {
+    std::cout << "Please enter a number greater than 0 or less than 50" << std::endl;
+    std::cin >> nbSolutions;
+  }
+
+  if (printDetails == 1) {
+    std::cout << "You have chosen to print the details of the kMedoid operations" << std::endl;
+  } else {
+    std::cout << "You have chosen not to print the details of the kMedoid operations" << std::endl;
+  }
+  std::cout << "You have chosen to generate " << nbSolutions << " solutions" << std::endl;
+
+  std::cout << "Press enter to continue";
+  fflush(stdin);
+  getchar();
+
   auto *dataColl = new dataCollection(path);
 
   auto *data1 = new data();
   auto *dataAnalysis1 = new dataAnalysis(dataColl, data1);
-  
+
   dataAnalysis1->dataAnalysisPrint(data1);
 
   auto *kMedoids1 = new kMedoids(data1);
@@ -44,69 +62,26 @@ int main(int argc, char *argv[]) {
 
   auto *genetic1 = new genetic();
 
-  auto *solution1 = new solution(data1, "Solution 1");
-  auto *solution2 = new solution(data1, "Solution 2");
-  auto *solution3 = new solution(data1, "Solution 3");
-  auto *solution4 = new solution(data1, "Solution 4");
-  auto *solution5 = new solution(data1, "Solution 5");
-  auto *solution6 = new solution(data1, "Solution 6");
-  auto *solution7 = new solution(data1, "Solution 7");
+  solution *bestSolution;
+  std::vector<solution *> solutions;
 
-
-  solution1->initialSolution(data1);
-
-
-  /* --------------------------------------- Mutation 1 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 1" << std::endl;
-  solution2->initialSolution(data1);
-
-
-  /* --------------------------------------- Mutation 2 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 2" << std::endl;
-  solution3->initialSolution(data1);
-
-
-  /* --------------------------------------- Mutation 3 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 3" << std::endl;
-  solution4->initialSolution(data1);
-
-
-  /* --------------------------------------- Mutation 4 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 4" << std::endl;
-  solution5->initialSolution(data1);
-
-  /* --------------------------------------- Mutation 5 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 4" << std::endl;
-  solution6->initialSolution(data1);
-
-  /* --------------------------------------- Mutation 6 --------------------------------------- */
-
-  data1 = genetic1->mutateData(data1);
-  std::cout << "Mutate data done 4" << std::endl;
-  solution7->initialSolution(data1);
+  for (int i = 0; i < nbSolutions; i++) {
+    solutions.push_back(new solution(data1, "Solution " + std::to_string(i)));
+    solutions[i]->initialSolution(data1);
+    data1 = genetic1->mutateData(data1);
+  }
 
   std::cout << "============================================================" << std::endl;
   std::cout << "Best solution" << std::endl;
 
 
+  bestSolution = bestSolution->compareSolutions(solutions, data1);
 
-  solution1 = solution1->compareSolutions({solution1, solution2, solution3, solution4, solution5, solution6, solution7}, data1);
-
-  solution1->printSolution(data1);
-  std::cout << "Number of affectations :" << solution1->getNBAffectations(data1) << std::endl;
-  std::cout << "Cost : " << solution1->getDistance(data1)*0.2 << std::endl;
-  std::cout << "Number of specialities : " << solution1->getNBSpeciality(data1) << std::endl;
-  printf("Press enter to continue");
+  bestSolution->printSolution(data1);
+  std::cout << "Number of affectations :" << bestSolution->getNBAffectations(data1) << std::endl;
+  std::cout << "Cost : " << bestSolution->getDistance(data1) * 0.2 << std::endl;
+  std::cout << "Number of specialities : " << bestSolution->getNBSpeciality(data1) << std::endl << std::endl;
+  std::cout << "Press enter to continue";
   getchar();
   return 0;
 }
