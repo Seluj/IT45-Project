d9acf4b0db7863bfc2f59f01846cb70d2fe3a05b
jules <jules.ferlin@gmail.com>
2023-06-15 07:51:47 +0200

    improve run.sh

--- run.sh
+++ run.sh
@@ -3,20 +3,36 @@
 MAIN=main
 ARG=$1
 
+
+echo -e "\n\n"
+echo "===================================================="
+echo "            Project by Céliane and Jules            "
+echo "                                                    "
+echo "                        P23                         "
+echo "                                                    "
+echo "                      Use help                      "
+echo "===================================================="
+echo -e "\n\n"
+
+echo "Press enter to continue"
+read -r
+echo -e "\n\n"
+
 if [ ! -f "$MAIN" ]; then
   echo "$MAIN does not exists"
-  echo "Would you like to compile the project? (yes or no)"
+  echo -e "Would you like to compile the project? (yes or no)\n"
   read -r answer
-  while [ "$answer" != "yes" ] && [ "$answer" != "no" ]
-  do
-    echo "You have to answer yes or no"
+  while [ "$answer" != "yes" ] && [ "$answer" != "no" ]; do
+    echo -e "You have to answer yes or no\n"
     read -r answer
   done
 
   if [ "$answer" == "yes" ]; then
     ./build.sh "clean"
   else
-    echo "ok"
+    echo "===================================================="
+    echo "                      ok, bye                       "
+    echo "===================================================="
     exit
   fi
 else
@@ -30,8 +46,16 @@ if [[ -z "$ARG" ]]; then
   ARG="./instances/200Missions-2centres/"
 fi
 
+if [ "$ARG" == "help" ]; then
+  echo "Usage: ./run.sh [path]"
+  echo "path: path to input files"
+  echo "If you do not pass any argument, the default path is ./instances/200Missions-2centres/"
+  exit
+fi
+
 if [ ! -d "$ARG" ]; then
   echo "$ARG does not exists"
+  echo "Use help for more information"
   exit
 fi
 
